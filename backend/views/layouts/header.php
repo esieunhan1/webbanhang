b54a590d2159e5187db3bf91e285c7b554106a00
D.V.B <bienduong8699@gmail.com>
2021-12-15 13:07:34 +0700

    chinh sua

--- backend/views/layouts/header.php
+++ backend/views/layouts/header.php
@@ -25,7 +25,8 @@ if (isset($_SESSION['user'])) {
                 <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
-                <a href="index3.html" class="nav-link">Home</a>
+                <?php $url_home = "index.php?controller=category&action=index";?>
+                <a href="<?php $url_home;?>" class="nav-link">Home</a>
             </li>
             <li class="nav-item d-none d-sm-inline-block">
                 <a href="#" class="nav-link">Contact</a>
@@ -92,7 +93,7 @@ if (isset($_SESSION['user'])) {
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
-        <a href="index3.html" class="brand-link">
+        <a href="<?php $url_homel;?>" class="brand-link">
             <img src="assets/uploads/<?php echo $avatar; ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
             <span class="brand-text font-weight-light">Admin</span>
         </a>
